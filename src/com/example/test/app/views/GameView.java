b1711bd037b8afdd71e6b96113755e7db2c6dc91
charles <charles.l.capps@gmail.com>
2012-09-24 14:41:45 -0700

    Draws a green circle, woot!

--- src/com/example/test/app/views/GameView.java
+++ src/com/example/test/app/views/GameView.java
@@ -2,6 +2,8 @@ package com.example.test.app.views;
 
 import android.content.Context;
 import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
 import android.util.AttributeSet;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
@@ -14,11 +16,13 @@ public class GameView extends SurfaceView implements SurfaceHolder.Callback {
 	public GameView(Context context, AttributeSet attrs) {
 		super(context, attrs);
         getHolder().addCallback(this);
-        gameThread = new GameThread(getHolder());
     }
 
     @Override
     public void surfaceCreated(SurfaceHolder surfaceHolder) {
+        gameThread = new GameThread(getHolder());
+        gameThread.run();
+
         //To change body of implemented methods use File | Settings | File Templates.
     }
 
@@ -37,6 +41,13 @@ public class GameView extends SurfaceView implements SurfaceHolder.Callback {
             super();
 
             Canvas c = sh.lockCanvas();
+
+            Paint mPaint = new Paint();
+            mPaint.setColor(Color.GREEN);
+            mPaint.setStrokeWidth(2);
+            c.drawCircle(150, 150, 80, mPaint);
+
+            sh.unlockCanvasAndPost(c);
         }
     }
 }
