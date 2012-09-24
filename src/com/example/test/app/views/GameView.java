7ee469a713e1d005df19004eb84c25c963646d17
charles <charles.l.capps@gmail.com>
2012-09-24 13:09:49 -0700

    Got slider working on top of a custom SurfaceView that is currently doing nothing :-)

--- src/com/example/test/app/views/GameView.java
+++ src/com/example/test/app/views/GameView.java
@@ -1,17 +1,42 @@
 package com.example.test.app.views;
 
 import android.content.Context;
+import android.graphics.Canvas;
 import android.util.AttributeSet;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
 import android.view.View;
 
-public class GameView extends View {
+public class GameView extends SurfaceView implements SurfaceHolder.Callback {
+
+    private GameThread gameThread;
 
 	public GameView(Context context, AttributeSet attrs) {
 		super(context, attrs);
-		// TODO Auto-generated constructor stub
-        }
+        getHolder().addCallback(this);
+        gameThread = new GameThread(getHolder());
+    }
 
-        public void onDraw() {
+    @Override
+    public void surfaceCreated(SurfaceHolder surfaceHolder) {
+        //To change body of implemented methods use File | Settings | File Templates.
+    }
 
+    @Override
+    public void surfaceChanged(SurfaceHolder surfaceHolder, int i, int i1, int i2) {
+        //To change body of implemented methods use File | Settings | File Templates.
+    }
+
+    @Override
+    public void surfaceDestroyed(SurfaceHolder surfaceHolder) {
+        //To change body of implemented methods use File | Settings | File Templates.
+    }
+
+    private class GameThread extends Thread {
+        public GameThread(SurfaceHolder sh) {
+            super();
+
+            Canvas c = sh.lockCanvas();
         }
+    }
 }
